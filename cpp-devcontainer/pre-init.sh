a141ded2d1f5301b42a3deff111e9be50b2a0b6f
yusufemresamur <yusufemresamur01>
2026-04-19 13:28:52 +0200

    add pre-init script for files and directories

---
+++ cpp-devcontainer/pre-init.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+files=( "$HOME/.netrc" "$HOME/.gitconfig" "$HOME/.bash_history" )
+directories=( "$HOME/.cache" )
+
+
+# Create files
+for file in "${files[@]}"; do
+    if [ ! -f "$file" ]; then
+        touch "${file}"
+        echo "Created file '${file}'"
+    else
+        echo "File '${file}' already exists."
+    fi
+done
+
+# Create directories
+for dir in "${directories[@]}"; do
+    if [ ! -d "$dir" ]; then
+        mkdir -p "${dir}"
+        echo "Created directory '${dir}'"
+    else
+        echo "Directory '${dir}' already exists."
+    fi
+done
