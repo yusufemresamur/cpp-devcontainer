a8169ee1963f58a155e5f849e9a6e4bac5711d47
yusufemresamur <yusufemresamur01>
2026-04-19 12:13:15 +0000

    persist bash history per project

--- cpp-devcontainer/entrypoint.sh
+++ cpp-devcontainer/entrypoint.sh
@@ -1,1 +1,9 @@
 #!/usr/bin/env bash
+
+set -e
+
+sudo chown -R $(whoami): /commandhistory
+touch /commandhistory/.bash_history
+
+SNIPPET="export PROMPT_COMMAND='history -a' && export HISTFILE=/commandhistory/.bash_history"
+echo "$SNIPPET" >> "$HOME/.bashrc"
